c1deceddb56be878f504384cc57d7e789e2e1e2d
GaelBuenoBarthe <gaelbueno@aol.com>
2024-08-02 12:39:30 +0200

    exo 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,29 @@
+package ex1;
+
+/**
+ * Fournit des méthodes de calcul de base.
+ */
+public class Calcul {
+
+	/**
+	 * Additionne deux entiers.
+	 *
+	 * @param a le premier entier
+	 * @param b le deuxième entier
+	 * @return la somme des deux entiers
+	 */
+	public int additionner(int a, int b) {
+		return a + b;
+	}
+
+	/**
+	 * Soustrait le deuxième entier du premier.
+	 *
+	 * @param a le premier entier
+	 * @param b le deuxième entier
+	 * @return la différence entre les deux entiers
+	 */
+	public int soustraire(int a, int b) {
+		return a - b;
+	}
+}
